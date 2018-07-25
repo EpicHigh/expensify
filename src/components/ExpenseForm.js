96fda6cf87e51c9589bedc8a68482a03034e6004
EpicHigh <prasit_tongpradit@windowslive.com>
2018-07-25 15:04:31 +0700

    Add Airbnb Date Picker component.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,26 +1,40 @@
 import React, { Component } from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import 'react-dates/initialize';
 
 export default class ExpenseForm extends Component {
   state = {
     description: "",
     note: "",
-    amount: 0
+    amount: "",
+    createdAt: moment(),
+	  calendarFocused: false
   };
 
   onDescriptionChange = event => {
-	  event.persist();
+    event.persist();
     this.setState({ description: event.target.value });
   };
 
   onAmountChange = event => {
-	  event.persist();
-    this.setState({ amount: event.target.value });
+    const amount = String(event.target.value);
+    amount.match(/^\d*(\.\d{0,2})?$/) && this.setState({ amount });
   };
 
   onNoteChange = event => {
     event.persist();
     this.setState({ note: event.target.value });
   };
+
+  onDateChange = createdAt => {
+    this.setState({ createdAt });
+  };
+
+	onFocusChange = ({ focused }) => {
+		this.setState({calendarFocused: focused})
+	};
 
   render() {
     return (
@@ -34,10 +48,18 @@ export default class ExpenseForm extends Component {
             onChange={this.onDescriptionChange}
           />
           <input
-            type="number"
+            type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense"
