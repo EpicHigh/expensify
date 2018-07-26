d74bdea4a2c6515f789fa2afcf6f6ccc420efeb0
EpicHigh <prasit_tongpradit@windowslive.com>
2018-07-27 00:52:46 +0700

    Clear index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,8 @@ import App from "./containers/App";
 import configureStore from "./store/configure";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import { addExpense } from "./actions/expenses";
-import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-store.dispatch(
-  addExpense({ description: "Water Bill", amount: 4500, createdAt: 4500 })
-);
-store.dispatch(
-  addExpense({ description: "Gas Bill", amount: 3000, createdAt: 1000 })
-);
-store.dispatch(
-  addExpense({ description: "Rent", amount: 19500, createdAt: 3000 })
-);
-const state = store.getState();
-
-getVisibleExpenses(state.expenses, state.filters);
 
 ReactDOM.render(
   <Provider store={store}>
