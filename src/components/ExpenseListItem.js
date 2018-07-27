ace2bd1c5b6cc0f5a205e0a923b07ee5ab559868
EpicHigh <prasit_tongpradit@windowslive.com>
2018-07-27 17:49:46 +0700

    Edit a dashboard page.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,20 @@
 import React from "react";
+import moment from "moment";
+import numeral from "numeral";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+
+numeral.register("locale", "th", {
+	delimiters: {
+		thousands: ',',
+		decimal: '.'
+	},
+  currency: {
+    symbol: "฿"
+  }
+});
+
+numeral.locale("th");
 
 const ExpenseListItem = ({ description, amount, createdAt, _id }) => (
   <div>
@@ -8,7 +22,9 @@ const ExpenseListItem = ({ description, amount, createdAt, _id }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount).format("$0,0.00")}
+      -
+      {moment(createdAt).format("Do MMMM YYYY")}
     </p>
   </div>
 );
